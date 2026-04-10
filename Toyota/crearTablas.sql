8ee6e7a83db0855d82ef7d381e24f02c1e023a32
Edgar Steinberg <s.steinberg2019@gmail.com>
2026-04-09 22:29:55 -0300

    feat(db): agregar claves primarias y foráneas a las tablas

--- Toyota/crearTablas.sql
+++ Toyota/crearTablas.sql
@@ -1,24 +1,39 @@
+DROP TABLE IF EXISTS sucursales;
+DROP TABLE IF EXISTS versiones;
+DROP TABLE IF EXISTS modelos;
+
+
+-- TABLA MODELOS
 CREATE TABLE modelos(
-	id_models INT,
-	nombre VARCHAR(50),
-	tipo VARCHAR(50)
+id_modelo INT PRIMARY KEY,
+nombre VARCHAR(50),
+tipo VARCHAR(30)
 );
+
 SELECT * FROM modelos;
+-- TABLA VERSIONES
 
 CREATE TABLE versiones(
-	id_version INT,
-	id_modelo INT,
-	nombre VARCHAR(50),
-	motor VARCHAR(30),
-	precio DECIMAL(12,2)
+id_version INT PRIMARY KEY,
+id_modelo INT ,
+nombre VARCHAR(50),
+motor VARCHAR(30),
+precio DECIMAL(12,2),
+FOREIGN KEY(id_modelo)
+	REFERENCES modelos(id_modelo)
 );
-SELECT * FROM versiones;
+
+SELECT * FROM modelos;
+-- TABLA SUCURSALES
 
 CREATE TABLE sucursales(
-	id_sucursal INT,
-	nombre VARCHAR(50),
-	ciudad VARCHAR(50),
-	id_version INT,
-	stock INT
+id_sucursal INT PRIMARY KEY,
+nombre VARCHAR(50),
+ciudad VARCHAR(50),
+id_version INT,
+stock INT,
+FOREIGN KEY(id_version)
+	REFERENCES versiones(id_version)
 );
+
 SELECT * FROM sucursales;
